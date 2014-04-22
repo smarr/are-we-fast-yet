1f6585d163d8eac32ee32f31c159ab660f037eb0
Stefan Marr <git@stefan-marr.de>
2014-04-22 14:36:21 +0200

    Added build script for Graal
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ implementations/build-graal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ./script.inc
+source ./config.inc
+
+([ -z $CXX ] && c++ --version || $CXX --version) | grep -q clang-5
+if [ $? -eq 0 ]; then
+    # we have to make clang compile graal gracefully
+    export COMPILER_WARNINGS_FATAL=false
+    export USE_PRECOMPILED_HEADER=0
+    export USE_CLANG=true
+fi
+export DEFAULT_VM=server
+export JAVA_HOME=$JAVA8_HOME
+export EXTRA_JAVA_HOMES=$JAVA7_HOME
+
+INFO Build Graal
+cd graal
+./mx.sh --vm server build
+./mx.sh --vm server trufflejar
+OK Graal Build Completed.
