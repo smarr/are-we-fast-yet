f45bb03a3582c1e7b75aa18ac2e766a18a39d028
Stefan Marr <git@stefan-marr.de>
2014-04-23 11:50:08 +0200

    Helper libraries
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ scripts/libraries.R
@@ -0,0 +1,9 @@
+writeLines("[INFO] Loading Libraries")
+
+library(plyr)
+library(ggplot2)
+library(psych)   # uses only geometric.mean
+
+source("data-processing.R")
+source("plots.R")
+source("colors.R")
