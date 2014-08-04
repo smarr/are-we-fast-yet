0112abe4af4bfd22d278afc85e5ff0fcb55fbd1a
Stefan Marr <git@stefan-marr.de>
2014-08-04 11:32:16 +0200

    Update path to retrieve data
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp 8:Projects/ARE-WE-THERE-YET/benchmark-setup/are-we-there-yet.data data/
+scp 8:Projects/ZERO-OVERHEAD-MOP/zero-overhead.data data/
 ssh 8 'bash -s' < scripts/spec.sh >& data/spec.md
