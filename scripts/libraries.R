2ff256d10f9a87c9348cf989f80fdd430f9bd8ab
Stefan Marr <git@stefan-marr.de>
2014-09-22 17:13:09 +0200

    Added lib install helper
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/libraries.R
+++ scripts/libraries.R
@@ -1,9 +1,17 @@
 writeLines("[INFO] Loading Libraries")
 
-library(plyr)
-library(ggplot2)
-library(psych)   # uses only geometric.mean
-library(tables)
+load_and_install_if_necessary <- function(package_name) {
+  if (!suppressPackageStartupMessages(library(package_name, character.only=TRUE, logical.return=TRUE))) {
+    cat(paste0("Package ", package_name, " not found. Will install it."))
+    install.packages(package_name)
+    library(package_name, character.only=TRUE)
+  }
+}
+
+load_and_install_if_necessary("plyr")
+load_and_install_if_necessary("ggplot2")
+load_and_install_if_necessary("psych")   # uses only geometric.mean
+load_and_install_if_necessary("tables")
 
 source("data-processing.R")
 source("plots.R")
