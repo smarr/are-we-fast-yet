445d1f8fba8fb66f8aa4f60e4346be66bc5ac5fa
Stefan Marr <git@stefan-marr.de>
2014-09-22 17:14:18 +0200

    Set margin to zero
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/plots.R
+++ scripts/plots.R
@@ -11,5 +11,6 @@ theme_simple <- function() {
           panel.grid.minor     = element_blank(),
           panel.border         = element_blank(),
           plot.background      = element_blank(), #element_rect(fill = NA, colour = NA)
-          strip.background     = element_blank()) 
+          strip.background     = element_blank(),
+          plot.margin = unit(c(0,0,0,0), "cm")) 
 }
