df082a988396c02492271fd9be3e890e04023e2f
Stefan Marr <git@stefan-marr.de>
2014-11-01 21:17:10 +0100

    Change build script to don't give a warning on my machine
    The `$(expr substr)` doesn't seem to work.
    So, make if and elif explicit to avoid it.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-jruby.sh
+++ implementations/build-jruby.sh
@@ -5,10 +5,21 @@ source ./config.inc
 
 pushd jruby
 INFO "Build JRuby+Truffle"
-[ "$(uname)" == "Darwin" ] && GRAAL=openjdk-8-graalvm-b132-macosx-x86_64-0.5.tar.gz
-[ "$(expr substr $(uname -s) 1 5)" == "Linux" ] && GRAAL=openjdk-8-graalvm-b132-linux-x86_64-0.5.tar.gz
+
+INFO Get Graal binary
+if [ "$(uname)" == "Darwin" ]
+then
+  GRAAL=openjdk-8-graalvm-b132-macosx-x86_64-0.5.tar.gz
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+  GRAAL=openjdk-8-graalvm-b132-linux-x86_64-0.5.tar.gz
+fi
+
 [ ! -f $GRAAL ] && wget http://lafo.ssw.uni-linz.ac.at/graalvm/$GRAAL
 [ ! -d graalvm-jdk1.8.0 ] && tar -zxf $GRAAL
+INFO Got Graal binary
+
+INFO Compile JRuby
 mvn clean install package -Pbootstrap
 OK "JRuby+Truffle Build Completed."
 popd
