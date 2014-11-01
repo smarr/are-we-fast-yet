96eaf40c5db6e6cd4a6fe435bf3adc13d7546eec
Stefan Marr <git@stefan-marr.de>
2014-11-01 20:02:28 +0100

    Smaller font size for paper, and no margin
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,15 +1,16 @@
 theme_simple <- function() {
     theme_bw() +
-    theme(axis.text.x          = element_text(size = 12, lineheight=0.7),
+    theme(axis.text.x          = element_text(size = 9, lineheight=0.7),
           axis.title.x         = element_blank(),
-          axis.title.y         = element_text(size = 12),
-          axis.text.y          = element_text(size = 12),
+          axis.title.y         = element_text(size = 9),
+          axis.text.y          = element_text(size = 9),
           axis.line            = element_line(colour = "gray"),
-          plot.title           = element_text(size = 12),
+          plot.title           = element_text(size = 9),
           panel.background     = element_blank(), #element_rect(fill = NA, colour = NA),
           panel.grid.major     = element_blank(),
           panel.grid.minor     = element_blank(),
           panel.border         = element_blank(),
           plot.background      = element_blank(), #element_rect(fill = NA, colour = NA)
-          strip.background     = element_blank()) 
+          strip.background     = element_blank(),
+          plot.margin = unit(c(0,0,0,0), "cm")) 
 }
