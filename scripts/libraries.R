23d407f463322498816392649438af5d924467f3
Stefan Marr <git@stefan-marr.de>
2014-11-02 20:33:17 +0100

    Added missing tikzDevice and set default mirror
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/libraries.R
+++ scripts/libraries.R
@@ -3,7 +3,7 @@ writeLines("[INFO] Loading Libraries")
 load_and_install_if_necessary <- function(package_name) {
   if (!suppressPackageStartupMessages(library(package_name, character.only=TRUE, logical.return=TRUE))) {
     cat(paste0("Package ", package_name, " not found. Will install it."))
-    install.packages(package_name)
+    install.packages(package_name, repos="http://cran.rstudio.com/") # nothing specific about RStudio, but this should just work
     library(package_name, character.only=TRUE)
   }
 }
@@ -12,6 +12,7 @@ load_and_install_if_necessary("plyr")
 load_and_install_if_necessary("ggplot2")
 load_and_install_if_necessary("psych")   # uses only geometric.mean
 load_and_install_if_necessary("tables")
+load_and_install_if_necessary("tikzDevice")
 
 source("data-processing.R")
 source("plots.R")
