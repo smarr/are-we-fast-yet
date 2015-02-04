7e8f64e084031b6a2a14c4971e01441961458169
Stefan Marr <git@stefan-marr.de>
2015-02-04 20:14:46 +0100

    Updated to data set used in submission
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp 8:Projects/ZERO-OVERHEAD-MOP/zero-overhead.data data/
+scp 8:Projects/ZERO-OVERHEAD-MOP/zero-overhead.data.bz2 data/
 ssh 8 'bash -s' < scripts/spec.sh >& data/spec.md
