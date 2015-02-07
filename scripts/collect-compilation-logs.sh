35d04f06946a7537882097b865d42c8f20045f05
Stefan Marr <git@stefan-marr.de>
2015-02-07 15:47:50 +0100

    Uncomment collection of compilation logs for RTruffleSOM
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- scripts/collect-compilation-logs.sh
+++ scripts/collect-compilation-logs.sh
@@ -8,21 +8,21 @@ source $BASE_DIR/../implementations/script.inc
 
 INFO Start Collecting Traces from RTruffleSOM-OMOP
 
-#cd $BASE_DIR/../implementations/RTruffleSOM/
+cd $BASE_DIR/../implementations/RTruffleSOM/
 
-#for bench in DirectAdd DnuAdd DnuPerformAdd PerformAdd \
-#             ProxyAdd IndirectAdd
-#do
-#  PYPYLOG=jit-log-opt,jit-backend:$LOG_FOLDER/rpython/$bench.log ./RTruffleSOM-jit -cp Smalltalk:Examples/Benchmarks/DoesNotUnderstand Examples/Benchmarks/BenchmarkHarness.som $bench 100 0 1000
-#done
+for bench in DirectAdd DnuAdd DnuPerformAdd PerformAdd \
+            ProxyAdd IndirectAdd
+do
+ PYPYLOG=jit-log-opt,jit-backend:$LOG_FOLDER/rpython/$bench.log ./RTruffleSOM-jit -cp Smalltalk:Examples/Benchmarks/DoesNotUnderstand Examples/Benchmarks/BenchmarkHarness.som $bench 100 0 1000
+done
 
-#cd $BASE_DIR/../implementations/RTruffleSOM-OMOP/
+cd $BASE_DIR/../implementations/RTruffleSOM-OMOP/
 
-#for bench in AddDispatch AddFieldWrite DispatchEnforcedStd FieldRead GlobalRead ReqPrim \
-#             AddDispatchEnforced AddFieldWriteEnforced DispatchEnforced FieldReadEnforced GlobalReadEnforced ReqPrimEnforced
-#do
-#  PYPYLOG=jit-log-opt,jit-backend:$LOG_FOLDER/rpython/$bench.log ./RTruffleSOM-jit -cp Smalltalk:Examples/Benchmarks/OMOP Examples/Benchmarks/BenchmarkHarness.som $bench 100 0 1000
-#done
+for bench in AddDispatch AddFieldWrite DispatchEnforcedStd FieldRead GlobalRead ReqPrim \
+            AddDispatchEnforced AddFieldWriteEnforced DispatchEnforced FieldReadEnforced GlobalReadEnforced ReqPrimEnforced
+do
+ PYPYLOG=jit-log-opt,jit-backend:$LOG_FOLDER/rpython/$bench.log ./RTruffleSOM-jit -cp Smalltalk:Examples/Benchmarks/OMOP Examples/Benchmarks/BenchmarkHarness.som $bench 100 0 1000
+done
 
 INFO Start Collecting Hotspot Compilation Results
 
