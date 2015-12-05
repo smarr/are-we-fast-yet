5408219eeabad25e673bdc589f5f8ce00ebc6db5
Stefan Marr <git@stefan-marr.de>
2015-12-05 17:25:48 +0100

    Added benchmark.sh for build server
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ benchmark.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir -p data
+rebench -d --scheduler=random --without-nice rebench.conf all
+
+REV=`git rev-parse HEAD | cut -c1-8`
+TARGET_PATH=~/benchmark-results/are-we-fast-yet/$REV
+mkdir -p $TARGET_PATH
+cp data/* $TARGET_PATH/
