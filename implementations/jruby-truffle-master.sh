22c9284320d85057fe2254f511df6bda23cc1139
Stefan Marr <git@stefan-marr.de>
2015-12-14 21:05:10 +0100

    Used wrong JRuby in JRuby-truffle-master
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/jruby-truffle-master.sh
+++ implementations/jruby-truffle-master.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 GRAAL07_DIR=$SCRIPT_PATH/graalvm-jdk1.8.0
-source $SCRIPT_PATH/config.inc
-
 export JAVACMD=$GRAAL07_DIR/bin/java
-exec $JRUBY_CMD -X+T "$@"
+exec $SCRIPT_PATH/JRuby-master/bin/jruby -X+T "$@"
