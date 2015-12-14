bd5dba46298f134f2e79a0b7e2b2bd609a03756d
Stefan Marr <git@stefan-marr.de>
2015-12-14 01:19:20 +0100

    Number result folders for easier ordering
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 rebench -d --scheduler=random --without-nice rebench.conf all
 
+DATA_ROOT=~/benchmark-results/are-we-fast-yet
+
 REV=`git rev-parse HEAD | cut -c1-8`
 
-TARGET_PATH=~/benchmark-results/are-we-fast-yet/$REV
-LATEST=~/benchmark-results/are-we-fast-yet/latest
+NUM_PREV=`ls -l $DATA_ROOT | grep ^d | wc -l`
+NUM_PREV=`printf "%03d" $NUM_PREV`
+
+TARGET_PATH=$DATA_ROOT/$NUM_PREV-$REV
+LATEST=$DATA_ROOT/latest
 
 mkdir -p $TARGET_PATH
 cp benchmark.data $TARGET_PATH/
