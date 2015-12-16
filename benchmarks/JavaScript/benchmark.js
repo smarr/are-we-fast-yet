4a90ed834b2aeccec5cfadb0b42afa470ee83f12
Stefan Marr <git@stefan-marr.de>
2015-12-16 20:18:04 +0100

    Fix JavaScript benchmark harness so that inheritance works consistently
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/JavaScript/benchmark.js
+++ benchmarks/JavaScript/benchmark.js
@@ -1,24 +1,22 @@
 'use strict';
 
-function Benchmark() {
-  var that = this;
+function Benchmark() {}
 
-  this.innerBenchmarkLoop = function (innerIterations) {
-    for (var i = 0; i < innerIterations; i++) {
-      if (!that.verifyResult(that.benchmark())) {
-        return false;
-      }
+Benchmark.prototype.innerBenchmarkLoop = function (innerIterations) {
+  for (var i = 0; i < innerIterations; i++) {
+    if (!this.verifyResult(this.benchmark())) {
+      return false;
     }
-    return true;
-  };
+  }
+  return true;
+};
 
-  this.benchmark = function () {
-    throw "subclass responsibility"
-  };
+Benchmark.prototype.benchmark = function () {
+  throw "subclass responsibility"
+};
 
-  this.verifyResult = function () {
-    throw "subclass responsibility"
-  };
-}
+Benchmark.prototype.verifyResult = function () {
+  throw "subclass responsibility"
+};
 
 exports.Benchmark = Benchmark;
