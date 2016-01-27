32819b15c9930d555074f9558c1c859694359af4
Stefan Marr <git@stefan-marr.de>
2016-01-27 18:25:35 +0100

    Don't run Crystal separately anymore
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-rebench -d --scheduler=random --without-nice rebench.conf steady-crystal
 rebench -d --scheduler=random --without-nice rebench.conf all
 
 DATA_ROOT=~/benchmark-results/are-we-fast-yet
