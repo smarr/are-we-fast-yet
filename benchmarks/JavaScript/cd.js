50fb2ad60b0d0c685904ef5a15bde4aa8f0dfa94
Stefan Marr <git@stefan-marr.de>
2016-05-10 10:46:31 +0200

    Move code out of closure, to stay with in core language
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/JavaScript/cd.js
+++ benchmarks/JavaScript/cd.js
@@ -637,115 +637,113 @@ Node.prototype.successor = function () {
   return y;
 };
 
-var drawMotionOnVoxelMap = (function () {
-  var voxelSize = Constants.GOOD_VOXEL_SIZE;
-  var horizontal = new Vector2D(voxelSize, 0);
-  var vertical = new Vector2D(0, voxelSize);
+var voxelSize = Constants.GOOD_VOXEL_SIZE;
+var horizontal = new Vector2D(voxelSize, 0);
+var vertical = new Vector2D(0, voxelSize);
 
-  function voxelHash(position) {
-    var xDiv = (position.x / voxelSize) | 0;
-    var yDiv = (position.y / voxelSize) | 0;
-
-    var result = new Vector2D();
-    result.x = voxelSize * xDiv;
-    result.y = voxelSize * yDiv;
-
-    if (position.x < 0)
-      result.x -= voxelSize;
-    if (position.y < 0)
-      result.y -= voxelSize;
-
-    return result;
+function isInVoxel(voxel, motion) {
+  if (voxel.x > Constants.MAX_X ||
+    voxel.x < Constants.MIN_X ||
+    voxel.y > Constants.MAX_Y ||
+    voxel.y < Constants.MIN_Y) {
+    return false;
   }
 
-  return function (voxelMap, motion) {
-    var seen = new RedBlackTree();
+  var init = motion.posOne;
+  var fin = motion.posTwo;
 
-    function putIntoMap(voxel) {
-      var array = voxelMap.get(voxel);
-      if (!array) {
-        voxelMap.put(voxel, array = []);
-      }
-      array.push(motion);
-    }
+  var v_s = voxelSize;
+  var r = Constants.PROXIMITY_RADIUS / 2;
 
-    function isInVoxel(voxel) {
-      if (voxel.x > Constants.MAX_X ||
-        voxel.x < Constants.MIN_X ||
-        voxel.y > Constants.MAX_Y ||
-        voxel.y < Constants.MIN_Y) {
-        return false;
-      }
+  var v_x = voxel.x;
+  var x0 = init.x;
+  var xv = fin.x - init.x;
 
-      var init = motion.posOne;
-      var fin = motion.posTwo;
+  var v_y = voxel.y;
+  var y0 = init.y;
+  var yv = fin.y - init.y;
 
-      var v_s = voxelSize;
-      var r = Constants.PROXIMITY_RADIUS / 2;
+  var low_x, high_x;
+  low_x = (v_x - r - x0) / xv;
+  high_x = (v_x + v_s + r - x0) / xv;
 
-      var v_x = voxel.x;
-      var x0 = init.x;
-      var xv = fin.x - init.x;
+  if (xv < 0) {
+    var tmp = low_x;
+    low_x = high_x;
+    high_x = tmp;
+  }
 
-      var v_y = voxel.y;
-      var y0 = init.y;
-      var yv = fin.y - init.y;
+  var low_y, high_y;
+  low_y = (v_y - r - y0) / yv;
+  high_y = (v_y + v_s + r - y0) / yv;
 
-      var low_x, high_x;
-      low_x = (v_x - r - x0) / xv;
-      high_x = (v_x + v_s + r - x0) / xv;
+  if (yv < 0) {
+    var tmp = low_y;
+    low_y = high_y;
+    high_y = tmp;
+  }
 
-      if (xv < 0) {
-        var tmp = low_x;
-        low_x = high_x;
-        high_x = tmp;
-      }
+  return (((xv == 0 && v_x <= x0 + r && x0 - r <= v_x + v_s) /* no motion in x */ ||
+  ((low_x <= 1 && 1 <= high_x) || (low_x <= 0 && 0 <= high_x) ||
+  (0 <= low_x && high_x <= 1))) &&
+  ((yv == 0 && v_y <= y0 + r && y0 - r <= v_y + v_s) /* no motion in y */ ||
+  ((low_y <= 1 && 1 <= high_y) || (low_y <= 0 && 0 <= high_y) ||
+  (0 <= low_y && high_y <= 1))) &&
+  (xv == 0 || yv == 0 || /* no motion in x or y or both */
+  (low_y <= high_x && high_x <= high_y) ||
+  (low_y <= low_x && low_x <= high_y) ||
+  (low_x <= low_y && high_y <= high_x)));
+}
 
-      var low_y, high_y;
-      low_y = (v_y - r - y0) / yv;
-      high_y = (v_y + v_s + r - y0) / yv;
+function putIntoMap(voxelMap, voxel, motion) {
+  var array = voxelMap.get(voxel);
+  if (!array) {
+    voxelMap.put(voxel, array = []);
+  }
+  array.push(motion);
+}
 
-      if (yv < 0) {
-        var tmp = low_y;
-        low_y = high_y;
-        high_y = tmp;
-      }
+function voxelHash(position) {
+  var xDiv = (position.x / voxelSize) | 0;
+  var yDiv = (position.y / voxelSize) | 0;
 
-      return (((xv == 0 && v_x <= x0 + r && x0 - r <= v_x + v_s) /* no motion in x */ ||
-      ((low_x <= 1 && 1 <= high_x) || (low_x <= 0 && 0 <= high_x) ||
-      (0 <= low_x && high_x <= 1))) &&
-      ((yv == 0 && v_y <= y0 + r && y0 - r <= v_y + v_s) /* no motion in y */ ||
-      ((low_y <= 1 && 1 <= high_y) || (low_y <= 0 && 0 <= high_y) ||
-      (0 <= low_y && high_y <= 1))) &&
-      (xv == 0 || yv == 0 || /* no motion in x or y or both */
-      (low_y <= high_x && high_x <= high_y) ||
-      (low_y <= low_x && low_x <= high_y) ||
-      (low_x <= low_y && high_y <= high_x)));
-    }
+  var result = new Vector2D();
+  result.x = voxelSize * xDiv;
+  result.y = voxelSize * yDiv;
 
-    function recurse(nextVoxel) {
-      if (!isInVoxel(nextVoxel, motion)) {
-        return;
-      }
-      if (seen.put(nextVoxel, true)) {
-        return;
-      }
+  if (position.x < 0)
+    result.x -= voxelSize;
+  if (position.y < 0)
+    result.y -= voxelSize;
 
-      putIntoMap(nextVoxel);
+  return result;
+}
 
-      recurse(nextVoxel.minus(horizontal));
-      recurse(nextVoxel.plus(horizontal));
-      recurse(nextVoxel.minus(vertical));
-      recurse(nextVoxel.plus(vertical));
-      recurse(nextVoxel.minus(horizontal).minus(vertical));
-      recurse(nextVoxel.minus(horizontal).plus(vertical));
-      recurse(nextVoxel.plus(horizontal).minus(vertical));
-      recurse(nextVoxel.plus(horizontal).plus(vertical));
-    }
+function recurse(voxelMap, seen, nextVoxel, motion) {
+  if (!isInVoxel(nextVoxel, motion)) {
+    return;
+  }
+  if (seen.put(nextVoxel, true)) {
+    return;
+  }
 
-    recurse(voxelHash(motion.posOne));
-  };
-})();
+  putIntoMap(voxelMap, nextVoxel, motion);
+
+  recurse(voxelMap, seen, nextVoxel.minus(horizontal), motion);
+  recurse(voxelMap, seen, nextVoxel.plus(horizontal), motion);
+  recurse(voxelMap, seen, nextVoxel.minus(vertical), motion);
+  recurse(voxelMap, seen, nextVoxel.plus(vertical), motion);
+  recurse(voxelMap, seen, nextVoxel.minus(horizontal).minus(vertical), motion);
+  recurse(voxelMap, seen, nextVoxel.minus(horizontal).plus(vertical), motion);
+  recurse(voxelMap, seen, nextVoxel.plus(horizontal).minus(vertical), motion);
+  recurse(voxelMap, seen, nextVoxel.plus(horizontal).plus(vertical), motion);
+}
+
+
+function drawMotionOnVoxelMap(voxelMap, motion) {
+  var seen = new RedBlackTree();
+  recurse(voxelMap, seen, voxelHash(motion.posOne), motion);
+}
 
 function reduceCollisionSet(motions) {
   var voxelMap = new RedBlackTree();
