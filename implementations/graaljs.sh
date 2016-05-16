6d5a9b9858781e99c0ec606b4ac519733243ef45
Stefan Marr <git@stefan-marr.de>
2016-05-16 21:28:42 +0200

    Change graaljs.sh to use GraalVM release
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/graaljs.sh
+++ implementations/graaljs.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/config.inc
-echo $MX -p $GRAAL_JS --jdk jvmci --dynamicimport graal-enterprise node "$@"
-echo `pwd`
-exec $MX -p $GRAAL_JS --jdk jvmci --dynamicimport graal-enterprise node "$@"
+exec $GRAAL_JS "$@"
