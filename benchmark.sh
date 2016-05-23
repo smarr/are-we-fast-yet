78f0286a6e3e8c205203f8f3e1a86235e1424f2d
Stefan Marr <git@stefan-marr.de>
2016-05-23 21:28:12 +0200

    Fixed typo in experiment name
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 rebench -d --without-nice rebench.conf steady-java
 rebench -d --without-nice rebench.conf steady-crystal
 rebench -d --without-nice rebench.conf steady-js
-rebench -d --without-nice rebench.conf ruby-other
+rebench -d --without-nice rebench.conf ruby-others
 rebench -d --without-nice rebench.conf steady-som
 rebench -d --without-nice rebench.conf steady-ruby
 rebench -d --without-nice rebench.conf all
