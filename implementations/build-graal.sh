3648438ab18826feaaf4ce4f8a9996911a34f13f
Stefan Marr <git@stefan-marr.de>
2016-06-13 14:42:44 +0200

    Added Graal core to repo
    used by:
     - TruffleSOM
     - SOMns
     - JRuby+Truffle
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-graal.sh
+++ implementations/build-graal.sh
@@ -3,20 +3,10 @@
 source ./script.inc
 source ./config.inc
 
-([ -z $CXX ] && c++ --version || $CXX --version) | grep -q clang-5
-if [ $? -eq 0 ]; then
-    # we have to make clang compile graal gracefully
-    export COMPILER_WARNINGS_FATAL=false
-    export USE_PRECOMPILED_HEADER=0
-    export USE_CLANG=true
-fi
-export DEFAULT_VM=server
-export JAVA_HOME=$JAVA8_HOME
-export EXTRA_JAVA_HOMES=$JAVA7_HOME
-
 INFO Build Graal
-cd graal
-./mx.sh clean
-./mx.sh --vm server build -p
-#./mx.sh --vm server trufflejar
+cd graal-core
+../mx/mx sforceimports
+
+../mx/mx clean
+../mx/mx build -p
 OK Graal Build Completed.
