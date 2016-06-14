8515759208ed6666ada0f6304e6a05f3a0fc27af
Stefan Marr <git@stefan-marr.de>
2016-06-14 16:39:54 +0200

    Try harder to find latest node
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/node.sh
+++ implementations/node.sh
@@ -1,2 +1,11 @@
 #/bin/sh
-exec node "$@"
+if [ -x "`which nvm`" ]
+then
+  NODE_BIN=`nvm which 6`
+  if [ ! -x "$NODE_BIN" ] ; then
+    NODE_BIN=node
+  fi
+else
+  NODE_BIN=node
+fi
+exec $NODE_BIN "$@"
