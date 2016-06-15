79b57aa96e8fecd47aff9c2d22754a8ade94ccc0
Stefan Marr <git@stefan-marr.de>
2016-06-15 09:35:44 +0200

    Fix bugs in launcher scripts
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/graal-enterprise.sh
+++ implementations/graal-enterprise.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/config.inc
-exec $GRAAL_VM_CMD "$GRAAL_JIT_FLAGS $@"
+exec $GRAAL_VM_CMD $GRAAL_JIT_FLAGS "$@"
--- implementations/graal.sh
+++ implementations/graal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/config.inc
-exec $GRAAL_CORE_CMD "$GRAAL_JIT_FLAGS $@"
+exec $GRAAL_CORE_CMD $GRAAL_JIT_FLAGS "$@"
