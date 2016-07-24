bace33150ef9a155c98e14971ec0d928ae35a35e
Stefan Marr <git@stefan-marr.de>
2016-07-25 00:03:21 +0200

    [ST] Finish CI setup for Pharo
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-pharo.sh
+++ implementations/build-pharo.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+if [ "$1" = "style" ]
+then
+  exit 0
+fi
+
 set -e # make script fail on first error
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/script.inc
 source $SCRIPT_PATH/config.inc
 
-## Get Pharo
+
+
+INFO Get Pharo VM
+
 get_web_getter
 $GET get.pharo.org/vm50 || $GET get.pharo.org/vm50
 bash vm50
+
+INFO Get Pharo Image
+cd $SCRIPT_PATH/../benchmarks/Smalltalk
+$GET get.pharo.org/stable || $GET get.pharo.org/stable
+bash stable
+
+INFO Build Benchmarking Image
+$SCRIPT_PATH/pharo Pharo.image build-image.st
