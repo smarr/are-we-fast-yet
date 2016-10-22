db977a597afa3b46b17653692e12ee17c462acba
Stefan Marr <git@stefan-marr.de>
2016-10-23 00:56:44 +0200

    Enable build of graal
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/setup.sh
+++ implementations/setup.sh
@@ -16,26 +16,12 @@ $SCRIPT_PATH/build-crystal.sh
 $SCRIPT_PATH/build-jruby.sh
 $SCRIPT_PATH/build-som.sh
 $SCRIPT_PATH/build-somns.sh
-# $SCRIPT_PATH/build-graal.sh
+$SCRIPT_PATH/build-graal.sh
 $SCRIPT_PATH/build-pharo.sh
-
 
 if [ -e "$GRAALVM_ARCHIVE" ]; then
   mkdir -p $SCRIPT_PATH/graalvm
   tar xf $GRAALVM_ARCHIVE --strip-components 1 -C $SCRIPT_PATH/graalvm
 fi
-
-# get_jdk9ea # disabled since Nashorn is only user and not really supported currently
-
-# 
-
-# ./build-rpysom.sh
-# ./build-rtrufflesom.sh
-
-# ./build-sompp.sh
-# ./build-som.sh
-
-# ./build-csom.sh
-# ./build-luajit.sh
 
 OK done.
