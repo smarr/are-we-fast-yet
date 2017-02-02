e7c13a66abbb954b64e0d041d38cde43bab4f1b1
Tim Felgentreff <timfelgentreff@gmail.com>
2017-02-02 23:23:25 +0100

    add Topaz Ruby

---
+++ implementations/build-topaz.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e # make script fail on first error
+SCRIPT_PATH=`dirname $0`
+source $SCRIPT_PATH/script.inc
+
+INFO Build Topaz
+pushd $SCRIPT_PATH
+  git clone git://github.com/topazproject/topaz
+  pushd topaz
+    git checkout tim/features
+    pip install invoke
+    export TEST_TYPE=translate-jit-notest
+    invoke travis.install_requirements
+    invoke travis.run_tests
+  popd
+popd
+OK Topaz Build Completed.
