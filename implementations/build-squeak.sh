d2c821eaaa84da096d005229fb2863933b725831
Stefan Marr <git@stefan-marr.de>
2017-03-04 18:13:54 +0100

    Use itimer VM for Squeak, doesn’t need sudo
    And should be as good to measure impact of Cog’s compiler.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-squeak.sh
+++ implementations/build-squeak.sh
@@ -8,26 +8,17 @@ set -e # make script fail on first error
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_PATH/script.inc
 
-if [ ! -e "$SCRIPT_PATH/squeak.sh" ]; then
+if [ ! -e "$SCRIPT_PATH/sqcogspurlinux" ]; then
   INFO Get Squeak VM
   pushd $SCRIPT_PATH
   get_web_getter
   $GET http://ftp.squeak.org/5.1/Squeak5.1-16549-32bit/Squeak5.1-16549-32bit-All-in-One.zip
+  $GET https://github.com/OpenSmalltalk/opensmalltalk-vm/releases/download/201701281910/cog_linux32x86_squeak.cog.spur_201701281910_itimer.tar.gz
   unzip -q Squeak5.1-16549-32bit-All-in-One.zip
-  cp Squeak5.1*.app/Contents/Resources/Squeak5.1*.image ../benchmarks/Smalltalk/Squeak.image
-  cp Squeak5.1*.app/Contents/Resources/Squeak5.1*.changes ../benchmarks/Smalltalk/Squeak.changes
-  cp Squeak5.1*.app/Contents/Resources/Squeak*.sources ../benchmarks/Smalltalk/
-
-  # This can be dropped once https://github.com/travis-ci/travis-build/pull/879 is merged and in production
-  if [[ "$(uname -s)" = "Linux" ]]; then
-      wget -q https://raw.githubusercontent.com/hpi-swa/smalltalkCI/master/utils/set_rtprio_limit.c
-      gcc -o set_rtprio_limit set_rtprio_limit.c
-      chmod +x ./set_rtprio_limit
-      sudo ./set_rtprio_limit $$
-  else
-      echo "#!/bin/bash" > set_rtprio_limit
-      chmod +x set_rtprio_limit
-  fi
+  mv Squeak5.1*.app/Contents/Resources/Squeak5.1*.image   ../benchmarks/Smalltalk/Squeak.image
+  mv Squeak5.1*.app/Contents/Resources/Squeak5.1*.changes ../benchmarks/Smalltalk/Squeak.changes
+  mv Squeak5.1*.app/Contents/Resources/Squeak*.sources    ../benchmarks/Smalltalk/
+  tar xf cog_linux32x86_squeak.cog.spur_201701281910_itimer.tar.gz
   popd
 fi
 
