14dc29c26b8218984aa9bedefddabb820efdb162
Stefan Marr <git@stefan-marr.de>
2017-03-04 22:58:39 +0100

    Add RSqueak as submodule
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-rsqueak.sh
+++ implementations/build-rsqueak.sh
@@ -8,15 +8,34 @@ set -e # make script fail on first error
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_PATH/script.inc
 
-if [ ! -e "$SCRIPT_PATH/RSqueak.sh" ]; then
+load_submodule RSqueak
+
+# TODO: override PyPy of RSqueak with a fixed version once >5.6.0 was released
+# if [ \! -d pypy ]; then
+#   get_pypy
+#   ln -s $SCRIPT_PATH/pypy $SCRIPT_PATH/RSqueak/.build/pypy
+# fi
+
+INFO Build RSqueak
+
+export SDL_VIDEODRIVER=dummy
+pushd $SCRIPT_PATH/RSqueak/.build
+python ./download_dependencies.py
+pypy   ./build.py --batch
+popd
+
+if [ ! -e "$SCRIPT_PATH/../benchmarks/Smalltalk/RSqueak.image" ]; then
   INFO Get RSqueak Image
   pushd $SCRIPT_PATH
   get_web_getter
   $GET https://www.hpi.uni-potsdam.de/hirschfeld/artefacts/rsqueak/bundle/RSqueak.zip
-  unzip -q RSqueak.zip
-  cp RSqueak.app/Contents/Resources/RSqueak.image ../benchmarks/Smalltalk/RSqueak.image
-  cp RSqueak.app/Contents/Resources/RSqueak.changes ../benchmarks/Smalltalk/RSqueak.changes
-  cp RSqueak.app/Contents/Resources/Squeak*.sources ../benchmarks/Smalltalk/
+  unzip -q RSqueak.zip \
+           RSqueak.app/Contents/Resources/RSqueak.image \
+           RSqueak.app/Contents/Resources/RSqueak.changes \
+           RSqueak.app/Contents/Resources/SqueakV50.sources
+  mv RSqueak.app/Contents/Resources/RSqueak.image     ../benchmarks/Smalltalk/RSqueak.image
+  mv RSqueak.app/Contents/Resources/RSqueak.changes   ../benchmarks/Smalltalk/RSqueak.changes
+  mv RSqueak.app/Contents/Resources/SqueakV50.sources ../benchmarks/Smalltalk/
   popd
 fi
 
---
+++ implementations/rsqueak.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export SDL_VIDEODRIVER=dummy
+exec `dirname $0`/$SCRIPT_PATH/RSqueak/.build/rsqueak $@
