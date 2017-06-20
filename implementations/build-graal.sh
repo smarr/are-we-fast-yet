c80e414ce0146a1212509a41ae99d4c27b4c158b
Stefan Marr <git@stefan-marr.de>
2017-06-20 22:46:28 +0200

    Added GraalBasic repo, adapted setup
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-graal.sh
+++ implementations/build-graal.sh
@@ -5,11 +5,31 @@ source $SCRIPT_PATH/script.inc
 source $SCRIPT_PATH/config.inc
 
 INFO Build Graal
-load_submodule $SCRIPT_PATH/mx
-load_submodule $SCRIPT_PATH/graal-core
-cd $SCRIPT_PATH/graal-core
-../mx/mx sforceimports
+load_submodule $SCRIPT_PATH/graal
+cd $SCRIPT_PATH/graal
 
+cd graal-jvmci-8
 ../mx/mx clean
-../mx/mx build -p
+../mx/mx build
+
+export JVMCI_HOME=`../mx/mx --java-home ${JAVA8_HOME} jdkhome`
+
+cd ..
+
+echo ""
+echo %% Build Graal Compiler
+echo ""
+cd truffle/compiler
+../../mx/mx --java-home ${JVMCI_HOME} clean
+../../mx/mx --java-home ${JVMCI_HOME} build
+
+echo ""
+echo %% Deploy Graal Into JVMCI Built
+cp mxbuild/dists/graal.jar ${JVMCI_HOME}/jre/lib/jvmci/
+
+if [ -d $SCRIPT_PATH/graal-core ]; then
+  rm -Rf $SCRIPT_PATH/graal-core
+fi
+mv $JAVA_HOME $SCRIPT_PATH/graal-core
+
 OK Graal Build Completed.
