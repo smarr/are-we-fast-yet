5a5e65f3cc5053c963e899d704f2c0b7c729604f
Stefan Marr <git@stefan-marr.de>
2017-11-08 00:07:13 +0000

    Restore right invoke version
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-topaz.sh
+++ implementations/build-topaz.sh
@@ -9,7 +9,10 @@ pushd $SCRIPT_PATH
   pushd Topaz
     pip install invoke==0.15.0
     export TEST_TYPE=translate-jit-notest
+    # requirements don't fix invoke, so, make sure we have the right version
     invoke travis.install_requirements
+    pip install invoke
+    pip install invoke==0.15.0
     invoke travis.run_tests
   popd
 popd
