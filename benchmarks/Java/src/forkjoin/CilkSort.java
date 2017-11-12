feb5e972516c15a6fbaae0800deb7862ff39477c
Stefan Marr <git@stefan-marr.de>
2017-11-12 16:52:05 +0000

    [Java] Adapted CilkSort benchmark
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Java/src/forkjoin/CilkSort.java
+++ benchmarks/Java/src/forkjoin/CilkSort.java
@@ -1,3 +1,10 @@
+package forkjoin;
+
+import java.util.concurrent.RecursiveAction;
+
+import som.Benchmark;
+import som.Random;
+
 /*
  * Copyright (c) 2000 Massachusetts Institute of Technology
  * Copyright (c) 2000 Matteo Frigo
@@ -15,7 +22,6 @@
  * You should have received a copy of the GNU General Public License
  * aint with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 /*
@@ -58,290 +64,282 @@
  * Vivek Kumar: Ported to JavaTC work-asyncing.
  */
 
-import java.util.Random;
+/**
+ * Cilksort is a parallel sorting algorithm, donned "Multisort", which
+ * is a variant of ordinary mergesort.  Multisort begins by dividing an
+ * array of elements in half and sorting each half.  It then merges the
+ * two sorted halves back together, but in a divide-and-conquer approach
+ * rather than the usual serial merge.
+ */
+public final class CilkSort extends Benchmark {
+	private static final int KILO = 1024;
+	private static final int MERGESIZE = (2 * KILO);
+	private static final int QUICKSIZE = (2 * KILO);
 
-public class CilkSort {
-	public static final int KILO = 1024;
-	public static final int MERGESIZE = (2 * KILO);
-	public static final int QUICKSIZE = (2 * KILO);
-	public static int partition(final int left, final int right) {
-		int i = left;
-		int j = right;
-		int tmpx;
-		int pivot = array[(left + right) / 2];
-		while(i <= j){
-			while(array[i] < pivot) {
-        i++;
-      }
-			while(array[j] > pivot) {
-        j--;
-      }
-			if(i <= j) {
-				tmpx = array[i];
-				array[i] = array[j];
-				array[j] = tmpx;
-				i++;
-				j--;
-			}
-		}
-		return i;
+	private static final int SIZE = 10_000_000;
+
+	@Override
+  public Object benchmark() {
+    int[] array = new int[SIZE];
+    int[] tmp = new int[SIZE];
+
+    Random r = new Random();
+    for (int i = 0; i < SIZE; i++) {
+      array[i] = r.next();
+    }
+
+    new Sort(array, tmp, 0, 0, SIZE).compute();
+    return array;
 	}
 
-	public static void quicksort(final int left, final int right) {
-		final int index = partition(left, right);
-		if(left < index - 1) {
-      quicksort(left, index - 1);
-    }
-		if(index < right) {
-      quicksort(index, right);
-    }
-	}
+  @Override
+  public boolean verifyResult(final Object result) {
+    int[] array = (int[]) result;
+    int a = 0;
+    int b;
+    boolean ok = true;
 
-	public static void seqmerge(int low1, final int high1, int low2, final int high2, int lowdest, final int[] src, final int[] dest) {
+    for (int k = 0; k < SIZE; k++) {
+      b = array[k];
+      ok &= a <= b;
+      a = b;
+    }
+
+    return ok;
+  }
+
+	private static void seqmerge(int low1, final int high1, int low2, final int high2, int lowdest, final int[] src, final int[] dest) {
 		int a1;
 		int a2;
-		if(low1 < high1 && low2 < high2) {
+		if (low1 < high1 && low2 < high2) {
 			a1 = src[low1];
 			a2 = src[low2];
-			for(; true; ) {
-				if(a1 < a2) {
+
+			for (;;) {
+				if (a1 < a2) {
 					dest[lowdest++] = a1;
 					a1 = src[++low1];
-					if(low1 >= high1) {
-            break ;
+					if (low1 >= high1) {
+            break;
           }
 				}
 				else {
 					dest[lowdest++] = a2;
 					a2 = dest[++low2];
-					if(low2 >= high2) {
-            break ;
+					if (low2 >= high2) {
+            break;
           }
 				}
 			}
 		}
-		if(low1 <= high1 && low2 <= high2) {
+
+		if (low1 <= high1 && low2 <= high2) {
 			a1 = src[low1];
 			a2 = src[low2];
-			for(; true; ) {
-				if(a1 < a2) {
+
+			for (;;) {
+				if (a1 < a2) {
 					dest[lowdest++] = a1;
 					++low1;
-					if(low1 > high1) {
-            break ;
+					if (low1 > high1) {
+            break;
           }
 					a1 = src[low1];
-				}
-				else {
+				} else {
 					dest[lowdest++] = a2;
 					++low2;
-					if(low2 > high2) {
-            break ;
+					if (low2 > high2) {
+            break;
           }
 					a2 = src[low2];
 				}
 			}
 		}
-		if(low1 > high1) {
+
+		if (low1 > high1) {
 			System.arraycopy(src, low2, dest, lowdest, (high2 - low2 + 1));
-		}
-		else {
+		} else {
 			System.arraycopy(src, low1, dest, lowdest, (high1 - low1 + 1));
 		}
 	}
 
-	public static int binsplit(final int val, int low, int high, final int[] src) {
+	private static int binsplit(final int val, int low, int high, final int[] src) {
 		int mid;
-		while(low != high){
+		while (low != high){
 			mid = low + ((high - low + 1) >> 1);
-			if(val <= src[mid]) {
+			if (val <= src[mid]) {
         high = mid - 1;
       } else {
         low = mid;
       }
 		}
-		if(src[low] > val) {
+
+		if (src[low] > val) {
       return low - 1;
     } else {
       return low;
     }
 	}
 
-	public static void cilkmerge(int low1, int high1, int low2, int high2, final int lowdest, final int[] src, final int[] dest) {
-		int split1;
-		int split2;
-		int lowsize;
-		if(high2 - low2 > high1 - low1) {
-			{
-				final int tmp = low1;
-				low1 = low2;
-				low2 = tmp;
-			}
-			{
-				final int tmp = high1;
-				high1 = high2;
-				high2 = tmp;
-			}
-		}
-		if(high1 < low1) {
-			System.arraycopy(src, low2, dest, lowdest, (high2 - low2));
-			return ;
-		}
-		if(high2 - low2 < MERGESIZE) {
-			seqmerge(low1, high1, low2, high2, lowdest, dest, src);
-			return ;
-		}
-		split1 = ((high1 - low1 + 1) / 2) + low1;
-		split2 = binsplit(split1, low2, high2, src);
-		lowsize = split1 - low1 + split2 - low2;
-		dest[(lowdest + lowsize + 1)] = src[split1];
+	private static class Sort extends RecursiveAction {
+    private static final long serialVersionUID = 4210346684339668520L;
 
-		finish {
-			async {
-				cilkmerge(low1, split1 - 1, low2, split2, lowdest, src, dest);
-				cilkmerge(split1 + 1, high1, split2 + 1, high2, lowdest + lowsize + 2, src, dest);
-			}
-		}
+    private final int low;
+    private final int tmpx;
+    private final int size;
+
+    private final int[] array;
+    private final int[] tmp;
+
+    Sort(final int[] array, final int[] tmp, final int low, final int tmpx, final int size) {
+      this.low = low;
+      this.tmpx = tmpx;
+      this.size = size;
+
+      this.array = array;
+      this.tmp = tmp;
+    }
+
+    @Override
+    protected void compute() {
+      int quarter = size / 4;
+
+      if (size < QUICKSIZE) {
+        quicksort(low, low + size - 1);
+        return;
+      }
+
+      int A = low;
+      int tmpA = tmpx;
+      int B = A + quarter;
+      int tmpB = tmpA + quarter;
+      int C = B + quarter;
+      int tmpC = tmpB + quarter;
+      int D = C + quarter;
+      int tmpD = tmpC + quarter;
+
+      Sort taskA = new Sort(array, tmp, A, tmpA, quarter); taskA.fork();
+      Sort taskB = new Sort(array, tmp, B, tmpB, quarter); taskB.fork();
+      Sort taskC = new Sort(array, tmp, C, tmpC, quarter); taskC.fork();
+      Sort taskD = new Sort(array, tmp, D, tmpD, size - 3 * quarter); taskD.fork();
+
+      taskA.join();
+      taskB.join();
+      taskC.join();
+      taskD.join();
+
+      Merge mergeA = new Merge(A, A + quarter - 1, B, B + quarter - 1, tmpA,
+          array, tmp);
+      mergeA.fork();
+      Merge mergeB = new Merge(C, C + quarter - 1, D, low + size - 1, tmpC,
+          array, tmp);
+      mergeB.fork();
+
+      mergeA.join();
+      mergeB.join();
+
+      new Merge(tmpA, tmpC - 1, tmpC, tmpA + size - 1, A, tmp, array).compute();
+    }
+
+    private int partition(final int left, final int right) {
+      int i = left;
+      int j = right;
+      int tmpx;
+      int pivot = array[(left + right) / 2];
+      while (i <= j){
+        while (array[i] < pivot) {
+          i++;
+        }
+        while (array[j] > pivot) {
+          j--;
+        }
+        if (i <= j) {
+          tmpx = array[i];
+          array[i] = array[j];
+          array[j] = tmpx;
+          i++;
+          j--;
+        }
+      }
+      return i;
+    }
+
+    private void quicksort(final int left, final int right) {
+      final int index = partition(left, right);
+      if (left < index - 1) {
+        quicksort(left, index - 1);
+      }
+
+      if (index < right) {
+        quicksort(index, right);
+      }
+    }
 	}
 
-	public static void cilksort(final int low, final int tmpx, final int size) {
-		int quarter = size / 4;
-		int A;
-		int B;
-		int C;
-		int D;
-		int tmpA;
-		int tmpB;
-		int tmpC;
-		int tmpD;
-		if(size < QUICKSIZE) {
-			quicksort(low, low + size - 1);
-			return ;
-		}
-		A = low;
-		tmpA = tmpx;
-		B = A + quarter;
-		tmpB = tmpA + quarter;
-		C = B + quarter;
-		tmpC = tmpB + quarter;
-		D = C + quarter;
-		tmpD = tmpC + quarter;
+	private static class Merge extends RecursiveAction {
+    private static final long serialVersionUID = -6933250566167176280L;
 
-		finish {
-			async {
-				cilksort(A, tmpA, quarter);
-				cilksort(B, tmpB, quarter);
-				cilksort(C, tmpC, quarter);
-				cilksort(D, tmpD, size - 3 * quarter);
-			}
-		}
+    private int low1;
+	  private int high1;
+	  private int low2;
+	  private int high2;
+	  private final int lowdest;
+	  private final int[] src;
+	  private final int[] dest;
 
-		finish {
-			async {
-				cilkmerge(A, A + quarter - 1, B, B + quarter - 1, tmpA, array, tmp);
-				cilkmerge(C, C + quarter - 1, D, low + size - 1, tmpC, array, tmp);
-			}
-		}
+	  Merge(final int low1, final int high1, final int low2, final int high2,
+	      final int lowdest, final int[] src, final int[] dest) {
+	    this.low1    = low1;
+	    this.high1   = high1;
+	    this.low2    = low2;
+	    this.high2   = high2;
+	    this.lowdest = lowdest;
+	    this.src  = src;
+	    this.dest = dest;
+	  }
 
-		cilkmerge(tmpA, tmpC - 1, tmpC, tmpA + size - 1, A, tmp, array);
-	}
+    @Override
+    protected void compute() {
+      int split1;
+      int split2;
+      int lowsize;
 
-	public static void usage() {
-		System.out.println("========================================================================");
-		System.out.println("Cilksort is a parallel sorting algorithm, donned \"Multisort\", which");
-		System.out.println("is a variant of ordinary mergesort.  Multisort begins by dividing an");
-		System.out.println("array of elements in half and sorting each half.  It then merges the");
-		System.out.println("two sorted halves back together, but in a divide-and-conquer approach");
-		System.out.println("rather than the usual serial merge.");
-		System.out.println("========================================================================");
-	}
+      if (high2 - low2 > high1 - low1) {
+        {
+          final int tmp = low1;
+          low1 = low2;
+          low2 = tmp;
+        }
+        {
+          final int tmp = high1;
+          high1 = high2;
+          high2 = tmp;
+        }
+      }
 
-	public static int[] array;
-	public static int[] tmp;
+      if (high1 < low1) {
+        System.arraycopy(src, low2, dest, lowdest, (high2 - low2));
+        return;
+      }
 
-	public static void main(final String[] args) {
-		boolean check = false;
-		int size = 10000000; // 10 million
+      if (high2 - low2 < MERGESIZE) {
+        seqmerge(low1, high1, low2, high2, lowdest, dest, src);
+        return ;
+      }
 
-		if(args.length > 0) {
-      size = Integer.parseInt(args[0]);
+      split1 = ((high1 - low1 + 1) / 2) + low1;
+      split2 = binsplit(split1, low2, high2, src);
+      lowsize = split1 - low1 + split2 - low2;
+      dest[(lowdest + lowsize + 1)] = src[split1];
+
+      Merge mergeA = new Merge(low1, split1 - 1, low2, split2, lowdest, src, dest);
+      mergeA.fork();
+
+      Merge mergeB = new Merge(split1 + 1, high1, split2 + 1, high2, lowdest + lowsize + 2, src, dest);
+      mergeB.fork();
+
+      mergeA.join();
+      mergeB.join();
     }
-		if(args.length > 1) {
-      check = Boolean.parseBoolean(args[1]);
-    }
-
-		int inner = 5;
-		int outter = 3;
-		if(args.length > 2) {
-      inner = Integer.parseInt(args[2]);
-    }
-		if(args.length > 3) {
-      outter = Integer.parseInt(args[3]);
-    }
-
-		System.out.println("Size = "+size+" check = "+check);
-
-		array = new int[size];
-		tmp = new int[size];
-		final int[] backup = new int[size];
-		Random r = new Random(0);
-		for(int i = 0; i < size; i++) {
-			backup[i] = r.nextInt(999999);
-		}
-		usage();
-
-		boolean harnessStarted = false;
-		final long start = System.nanoTime();
-		for(int i=0;i <outter; i++) {
-			if(i+1 == outter) {
-				harnessStarted = true;
-				org.mmtk.plan.Plan.harnessBegin();
-				org.jikesrvm.scheduler.RVMThread.perfEventStart();
-			}
-			for(int j=0; j<inner; j++) {
-				System.out.println("========================== ITERATION ("+i+"."+j+") ==================================");
-				System.arraycopy(backup, 0, array, 0, size); // restore the old array
-				final long startTime = System.currentTimeMillis();
-				cilksort(0, 0, size);
-				final long time = System.currentTimeMillis() - startTime;
-				final double secs = (time) / 1000.0D;
-				if(harnessStarted) {
-					if(size == 10000000) {
-						if(org.jikesrvm.scheduler.WS.wsTotalPush() != 34105) {
-							System.out.println("EXITING DUE TO FAILURE IN HARNESS ITERATIONS");
-							System.exit(-1);
-						}
-					}
-				}
-				if(check) {
-					int a = 0;
-					int b;
-					boolean ok = true;
-					for(int k = 0; k < size; k++) {
-						b = array[k];
-						ok &= (a <= b);
-						a = b;
-					}
-					if(ok) {
-						System.out.println("CilkSort (" + size + "): passed. Time = " + secs);
-					}
-					else {
-						System.out.println("CilkSort (" + size + "): failed. Time = " + secs);
-					}
-				}
-				else {
-					System.out.println("Time = " + secs);
-				}
-				org.jikesrvm.scheduler.WS.dumpWSStatistics();
-			}
-		}
-
-		System.out.println("Test Kernel under harness passed successfully....");
-
-		org.jikesrvm.scheduler.RVMThread.perfEventStop();
-		org.mmtk.plan.Plan.harnessEnd();
-
-		final double duration = ((System.nanoTime() - start)/((1.0E9))) * 1000;
-		System.out.printf("===== Test PASSED in %d msec =====\n",(int)duration);
 	}
 }
