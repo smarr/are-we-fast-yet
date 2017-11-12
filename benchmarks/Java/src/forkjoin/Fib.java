9e7bff7d63391678c8c0c3aabe1224778b3e4965
Stefan Marr <git@stefan-marr.de>
2017-11-12 15:14:26 +0000

    [Java] Adapted Fib benchmark
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Java/src/forkjoin/Fib.java
+++ benchmarks/Java/src/forkjoin/Fib.java
@@ -1,72 +1,51 @@
-public class Fib {
-	private static int[] results = {
-		0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,
-		46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,
-		14930352,24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170
+package forkjoin;
+
+import java.util.concurrent.RecursiveTask;
+
+import som.Benchmark;
+
+public final class Fib extends Benchmark {
+  private static final int N = 40;
+
+	private static final int[] RESULTS = {
+		0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,
+		17711,28657, 46368,75025,121393,196418,317811,514229,832040,1346269,2178309,
+		3524578,5702887,9227465, 14930352,24157817,39088169,63245986,102334155,
+		165580141,267914296,433494437,701408733,1134903170
 	};
 
-	public static void main(final String args[]) {
-		int n = 40;
-
-		if (args.length > 0) {
-      n = Integer.parseInt(args[0]);
-    }
-
-		System.out.println("N = "+n);
-
-		boolean harnessStarted = false;
-
-		int l_start=1;
-		int inner = 5;
-		int outter = 3;
-		if(args.length > l_start) {
-      inner = Integer.parseInt(args[l_start]);
-    }
-		if(args.length > (l_start+1)) {
-      outter = Integer.parseInt(args[l_start+1]);
-    }
-
-		final long start = System.nanoTime();
-		for(int i=0;i <outter; i++) {
-			if(i+1 == outter) {
-				harnessStarted = true;
-				org.mmtk.plan.Plan.harnessBegin();
-				org.jikesrvm.scheduler.RVMThread.perfEventStart();
-			}
-			for(int j=0; j<inner; j++) {
-				System.out.println("========================== ITERATION ("+i+"."+j+") ==================================");
-				final long startTime = System.currentTimeMillis();
-				int r = fib(n);
-				final long time = System.currentTimeMillis() - startTime;
-				final double secs = (time) / 1000.0;
-				if(harnessStarted) {
-					if(r != results[n]) {
-						System.out.println("EXITING DUE TO FAILURE IN HARNESS ITERATIONS");
-						System.exit(-1);
-					}
-				}
-				System.out.println(" Time: " + secs);
-				org.jikesrvm.scheduler.WS.dumpWSStatistics();
-			}
-		}
-
-		org.jikesrvm.scheduler.RVMThread.perfEventStop();
-		org.mmtk.plan.Plan.harnessEnd();
-
-		final double duration = ((System.nanoTime() - start)/((1.0E9))) * 1000;
-		System.out.printf("===== Test PASSED in %d msec =====\n",(int)duration);
+	@Override
+  public Object benchmark() {
+	  return new Fibonacci(N).compute();
 	}
 
-	private static int fib(final int n) {
-		int a = 0;
-		int b = 0;
-		if (n <= 2) return 1;
-		finish {
-			async {
-				a = fib(n-1);
-				b = fib(n-2);
-			}
-		}
-		return a + b;
+	@Override
+  public boolean verifyResult(final Object result) {
+	  int r = (int) result;
+	  return r == RESULTS[N];
+	}
+
+	private final static class Fibonacci extends RecursiveTask<Integer> {
+    private static final long serialVersionUID = -2556124042120695959L;
+
+    private final int n;
+
+    public Fibonacci(final int n) {
+      this.n = n;
+    }
+
+    @Override
+    protected Integer compute() {
+      if (n <= 2) {
+        return 1;
+      }
+
+      Fibonacci taskA = new Fibonacci(n - 1);
+      taskA.fork();
+      Fibonacci taskB = new Fibonacci(n - 2);
+      taskB.fork();
+
+      return taskA.join() + taskB.join();
+    }
 	}
 }
