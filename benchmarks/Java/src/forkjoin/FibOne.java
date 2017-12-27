b020c2f86345bea232f3541a54134af5b7117635
Stefan Marr <git@stefan-marr.de>
2017-12-27 16:33:59 +0100

    Make fib problem size configurable, format code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Java/src/forkjoin/FibOne.java
+++ benchmarks/Java/src/forkjoin/FibOne.java
@@ -7,27 +7,35 @@ import som.Benchmark;
 // Only one active task at any given time.
 
 public final class FibOne extends Benchmark {
-  private static final int N = 33;
 
-	private static final int[] RESULTS = {
-		0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,
-		17711,28657, 46368,75025,121393,196418,317811,514229,832040,1346269,2178309,
-		3524578,5702887,9227465, 14930352,24157817,39088169,63245986,102334155,
-		165580141,267914296,433494437,701408733,1134903170
-	};
+  private static final int[] RESULTS = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+      89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657,
+      46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309,
+      3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986,
+      102334155, 165580141, 267914296, 433494437, 701408733, 1134903170 };
 
-	@Override
+  @Override
+  public boolean innerBenchmarkLoop(final int n) {
+    int result = new Fibonacci(n).compute();
+    return verifyResult(result, n);
+  }
+
+  @Override
   public Object benchmark() {
-	  return new Fibonacci(N).compute();
-	}
+    throw new UnsupportedOperationException();
+  }
 
-	@Override
+  @Override
   public boolean verifyResult(final Object result) {
-	  int r = (int) result;
-	  return r == RESULTS[N];
-	}
+    throw new UnsupportedOperationException();
+  }
 
-	private final static class Fibonacci extends RecursiveTask<Integer> {
+  public boolean verifyResult(final int r, final int n) {
+    return r == RESULTS[n];
+  }
+
+  private final static class Fibonacci extends RecursiveTask<Integer> {
+
     private static final long serialVersionUID = -2556124042120695959L;
 
     private final int n;
@@ -52,5 +60,5 @@ public final class FibOne extends Benchmark {
 
       return a + b;
     }
-	}
+  }
 }
