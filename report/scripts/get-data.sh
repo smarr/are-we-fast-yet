496b3f731e2b7820186d0298b0134be763cb8257
Stefan Marr <git@stefan-marr.de>
2018-04-23 17:07:19 +0100

    Make get-data location robust
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- report/scripts/get-data.sh
+++ report/scripts/get-data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-mkdir -p data/all
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd`
+popd > /dev/null
+
+mkdir -p $SCRIPT_PATH/../data/all
 # scp 8:~/benchmark-results/are-we-fast-yet/latest/* data/
-rsync -av 8:/home/gitlab-runner/benchmark-results/are-we-fast-yet/ data/all/
-ssh 8 'bash -s' < scripts/spec.sh >& data/spec.md
+rsync -av 8:/home/gitlab-runner/benchmark-results/are-we-fast-yet/ $SCRIPT_PATH/../data/all/
+ssh 8 'bash -s' < $SCRIPT_PATH/spec.sh >& $SCRIPT_PATH/../data/spec.md
