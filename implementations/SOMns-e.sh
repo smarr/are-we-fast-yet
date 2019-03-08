f4a595bf8488c54a5a9ffcd540e95023da33650c
Stefan Marr <git@stefan-marr.de>
2019-03-08 11:46:38 +0000

    Use external graal for SOMns-e
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/SOMns-e.sh
+++ implementations/SOMns-e.sh
@@ -3,4 +3,4 @@ SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/config.inc
 export JVMCI_BIN=${GRAALVM_HOME}/bin/java
 export GRAAL_FLAGS=$GRAAL_HOSTED_FLAGS
-exec $SCRIPT_PATH/SOMns/fast "$@"
+exec $SCRIPT_PATH/SOMns/fast -E "$@"
