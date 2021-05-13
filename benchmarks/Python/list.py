566385040a86e4f5d6fe47ba3638647e34543d8d
Stefan Marr <git@stefan-marr.de>
2021-05-13 17:01:08 +0100

    Enable pylint
    - added standard .pylintrc file
    - disable documentation pylint rules
    - fix basic style issues
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Python/list.py
+++ benchmarks/Python/list.py
@@ -6,42 +6,40 @@ class Element:
         self.val = v
 
     def length(self):
-        if self.next == None:
+        if self.next is None:
             return 1
-        else:
-            return 1 + self.next.length()
+        return 1 + self.next.length()
 
 
-def isShorterThan(x, y):
-    xTail = x
-    yTail = y
+def is_shorter_than(x, y):
+    x_tail = x
+    y_tail = y
 
-    while yTail != None:
-        if xTail == None:
+    while y_tail is not None:
+        if x_tail is None:
             return True
 
-        xTail = xTail.next
-        yTail = yTail.next
+        x_tail = x_tail.next
+        y_tail = y_tail.next
 
     return False
 
 
 def tail(x, y, z):
-    if isShorterThan(y, x):
+    if is_shorter_than(y, x):
         return tail(tail(x.next, y, z), tail(y.next, z, x), tail(z.next, x, y))
-    else:
-        return z
+    return z
 
 
-def makeList(length):
+def make_list(length):
     if length == 0:
         return None
-    else:
-        e = Element(length)
-        e.next = makeList(length - 1)
 
-        return e
+    e = Element(length)
+    e.next = make_list(length - 1)
+
+    return e
 
 
-result = tail(makeList(15), makeList(10), makeList(6))
+result = tail(make_list(15), make_list(10), make_list(6))
 print(result.length())
