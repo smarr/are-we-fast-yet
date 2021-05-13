ca1a3bb42a19e534ff1db0a0f87d87dce046999d
Stefan Marr <git@stefan-marr.de>
2021-05-13 16:37:30 +0100

    Added Python CI
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ implementations/build-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e # make script fail on first error
+SCRIPT_PATH=`dirname $0`
+source $SCRIPT_PATH/script.inc
+
+if [ "$1" = "style" ]
+then
+  INFO Check style of Python benchmarks
+  pushd $SCRIPT_PATH/../benchmarks/Python
+  python -m black --check --diff .
+  python -m pylint *.py
+else
+  exit 0
+fi
