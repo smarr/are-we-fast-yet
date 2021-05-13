a5d897260906da80add04e183647754eb2f86e0a
Stefan Marr <git@stefan-marr.de>
2021-05-13 16:55:16 +0100

    Fix formatting with black
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Python/list.py
+++ benchmarks/Python/list.py
@@ -6,7 +6,7 @@ class Element:
         self.val = v
 
     def length(self):
-        if (self.next == None):
+        if self.next == None:
             return 1
         else:
             return 1 + self.next.length()
@@ -16,36 +16,32 @@ def isShorterThan(x, y):
     xTail = x
     yTail = y
 
-    while (yTail != None):
-       if (xTail == None):
-         return True
+    while yTail != None:
+        if xTail == None:
+            return True
 
-
-       xTail = xTail.next
-       yTail = yTail.next
-
+        xTail = xTail.next
+        yTail = yTail.next
 
     return False
 
 
 def tail(x, y, z):
-    if (isShorterThan(y, x)):
-      return tail(
-        tail(x.next, y, z),
-        tail(y.next, z, x),
-        tail(z.next, x, y)
-      )
+    if isShorterThan(y, x):
+        return tail(tail(x.next, y, z), tail(y.next, z, x), tail(z.next, x, y))
     else:
-      return z
+        return z
+
 
 def makeList(length):
-    if (length == 0):
+    if length == 0:
         return None
     else:
         e = Element(length)
         e.next = makeList(length - 1)
 
         return e
+
 
 result = tail(makeList(15), makeList(10), makeList(6))
 print(result.length())
--- benchmarks/Python/sieve.py
+++ benchmarks/Python/sieve.py
@@ -1,15 +1,16 @@
 def sieve(flags, size):
     primeCount = 0
 
-    for i in range(2, size+1):
-      if (flags[i - 1]):
-        primeCount += 1
-        k = i + i
-        while (k <= size):
-          flags[k - 1] = False
-          k += i
+    for i in range(2, size + 1):
+        if flags[i - 1]:
+            primeCount += 1
+            k = i + i
+            while k <= size:
+                flags[k - 1] = False
+                k += i
 
     return primeCount
+
 
 flags = []
 for i in range(0, 5000):
