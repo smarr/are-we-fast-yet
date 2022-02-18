622934be34e2f50e91245667779e6c70076da536
Stefan Marr <git@stefan-marr.de>
2022-02-18 18:13:32 +0000

    Extract code for computing stats
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- report/scripts/data-processing.R
+++ report/scripts/data-processing.R
@@ -99,3 +99,106 @@ map_names <- function(old_names, name_map) {
   }
   old_names
 }
+
+## Function to calculate the needed statistics
+select_data <- function(data, filter_cond) {
+  filter_cond <- enquo(filter_cond)
+  data %>%
+    group_by(exe, bench,
+             varvalue, cores, inputsize, extraargs) %>%
+    select(!c(cmdline, commitid, trialid, suite, warmup, criterion)) %>%
+    filter(!!filter_cond) %>%
+    droplevels()
+}
+
+compute_baseline <- function(data, baseline_exe) {
+  data %>%
+    filter(exe == baseline_exe) %>%
+    summarise(base_median = median(value),
+              .groups = "drop")  %>%
+    select(!exe) %>%
+    droplevels()
+}
+
+compute_normalized <- function(data, baseline_data) {
+  data %>%
+    left_join(
+      baseline_data,
+      by = c("bench", "varvalue", "cores", "inputsize", "extraargs")) %>%
+    group_by(
+      exe, bench,
+      varvalue, cores, inputsize, extraargs) %>%
+    transform(ratio_median = value / base_median)
+}
+
+compute_bench_stats <- function(data) {
+  data %>%
+    group_by(
+      exe, bench,
+      varvalue, cores, inputsize, extraargs) %>%
+    summarise(
+      unit = unit[1],
+      min = min(value),
+      max = max(value),
+      median = median(value),
+      samples = length(value),
+      
+      ratio = median(value) / base_median[1],
+      change_m = ratio - 1,
+      .groups = "drop")
+}
+
+compute_exe_stats <- function(data) {
+  data %>%
+    group_by(exe) %>%
+    summarise(
+      unit = unit[1],
+      median_ratio = median(ratio),
+      min_ratio = min(ratio),
+      max_ratio = max(ratio),
+      .groups = "drop")
+}
+
+add_exe_median_ratio <- function (stats, stats_exe) {
+  d <- stats_exe %>%
+    rename(exe_ratio = median_ratio) %>%
+    select(exe, exe_ratio)
+  
+  stats %>%
+    left_join(d, by = c("exe"))
+}
+
+compute_all <- function(data, filter_cond, baseline) {
+  filter_cond <- enquo(filter_cond)
+  
+  filtered <- data %>%
+    select_data(!!filter_cond)
+  
+  base <- filtered %>%
+    compute_baseline(baseline)
+  
+  norm <- filtered %>%
+    compute_normalized(base)
+  
+  stats_bench <- norm %>%
+    compute_bench_stats()
+  
+  stats_exe <- stats_bench %>%
+    compute_exe_stats()
+  
+  stats_bench <- stats_bench %>%
+    add_exe_median_ratio(stats_exe)
+  
+  list(
+    filtered = filtered,
+    base = base,
+    normalized = norm,
+    baseline = baseline,
+    stats = list(
+      bench = stats_bench,
+      exe = stats_exe
+    )
+  )
+}
+
+
