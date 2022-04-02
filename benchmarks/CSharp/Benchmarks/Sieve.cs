014a44ec9a41d3a308eeea3a1c88c21cd2f7c6e8
Julius F. Heins <jfheins@gmail.com>
2022-04-02 21:31:41 +0200

    Move benchmarks into subfolder

---
+++ benchmarks/CSharp/Benchmarks/Sieve.cs
@@ -0,0 +1,35 @@
+public class Sieve : Benchmark
+{
+
+  public override object Execute()
+  {
+    bool[] flags = new bool[5000];
+    Array.Fill(flags, true);
+    return sieve(flags, 5000);
+  }
+
+  int sieve(bool[] flags, int size)
+  {
+    int primeCount = 0;
+
+    for (int i = 2; i <= size; i++)
+    {
+      if (flags[i - 1])
+      {
+        primeCount++;
+        int k = i + i;
+        while (k <= size)
+        {
+          flags[k - 1] = false;
+          k += i;
+        }
+      }
+    }
+    return primeCount;
+  }
+
+  public override bool VerifyResult(object result)
+  {
+    return 669 == (int)result;
+  }
+}
