775da1c9e3434d7474cc3d0193f968e97e3fe118
Stefan Marr <git@stefan-marr.de>
2022-07-14 00:25:33 +0100

    Set ReSharper output format to text and only include .cs files
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-csharp.sh
+++ implementations/build-csharp.sh
@@ -5,7 +5,8 @@ INFO "Build C# Benchmarks"
 pushd $SCRIPT_PATH/../benchmarks/CSharp
 if [ "$1" = "style" ]
 then
-  dotnet tool run jb inspectcode Benchmarks.csproj --build
+  dotnet tool run jb inspectcode Benchmarks.csproj --build -f=Text -o=report.txt --include=**/*.cs
+  cat report.txt
 else
   dotnet publish -c Release
 fi
