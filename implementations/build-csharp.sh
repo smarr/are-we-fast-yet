011a89f4480873c14d9f160e471b8364d8187d4f
Stefan Marr <git@stefan-marr.de>
2022-07-13 23:35:28 +0100

    Use ReSharper for C#
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- implementations/build-csharp.sh
+++ implementations/build-csharp.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 source $SCRIPT_PATH/script.inc
-INFO "Build C\# Benchmarks"
+INFO "Build C# Benchmarks"
 pushd $SCRIPT_PATH/../benchmarks/CSharp
 if [ "$1" = "style" ]
 then
-  WARN TODO
+  dotnet tool run jb inspectcode Benchmarks.csproj --build -f=Text -o=report.txt --include=**/*.cs
+  cat report.txt
 else
   dotnet publish -c Release
 fi
