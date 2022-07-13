b7bbe8f3685e28fe2f94f2175594904b46a7537c
Stefan Marr <git@stefan-marr.de>
2022-07-13 22:49:39 +0100

    Fix style and compilation issues
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/CSharp/Benchmark.cs
+++ benchmarks/CSharp/Benchmark.cs
@@ -1,13 +1,10 @@
-public abstract class Benchmark
-{
+public abstract class Benchmark {
     public abstract object Execute();
     public abstract bool VerifyResult(object result);
-    public virtual bool InnerBenchmarkLoop(int innerIterations)
-    {
-        for (int i = 0; i < innerIterations; i++)
-        {
-            if (!VerifyResult(Execute()))
-            {
+
+    public virtual bool InnerBenchmarkLoop(int innerIterations){
+        for(int i = 0; i < innerIterations; i++){
+            if(!VerifyResult(Execute())){
                 return false;
             }
         }
