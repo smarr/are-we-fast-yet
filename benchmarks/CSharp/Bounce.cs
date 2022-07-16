f331aa4fef718b7b9431ee37167801102dc77f55
Stefan Marr <git@stefan-marr.de>
2022-07-16 15:43:26 +0100

    [C#] Finish Bounce benchmark
    - make Ball private
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/CSharp/Bounce.cs
+++ benchmarks/CSharp/Bounce.cs
@@ -1,62 +1,62 @@
 namespace Benchmarks;
 
-public class Ball
-{
-  private int x;
-  private int y;
-  private int xVel;
-  private int yVel;
-
-  public Ball(Random random)
-  {
-    x = random.Next() % 500;
-    y = random.Next() % 500;
-    xVel = (random.Next() % 300) - 150;
-    yVel = (random.Next() % 300) - 150;
-  }
-
-  public bool Bounce()
-  {
-    int xLimit = 500;
-    int yLimit = 500;
-    bool bounced = false;
-
-    x += xVel;
-    y += yVel;
-    if (x > xLimit)
-    {
-      x = xLimit;
-      xVel = 0 - Math.Abs(xVel);
-      bounced = true;
-    }
-
-    if (x < 0)
-    {
-      x = 0;
-      xVel = Math.Abs(xVel);
-      bounced = true;
-    }
-
-    if (y > yLimit)
-    {
-      y = yLimit;
-      yVel = 0 - Math.Abs(yVel);
-      bounced = true;
-    }
-
-    if (y < 0)
-    {
-      y = 0;
-      yVel = Math.Abs(yVel);
-      bounced = true;
-    }
-
-    return bounced;
-  }
-}
-
 public sealed class Bounce : Benchmark
 {
+  private sealed class Ball
+  {
+    private int x;
+    private int y;
+    private int xVel;
+    private int yVel;
+
+    public Ball(Random random)
+    {
+      x = random.Next() % 500;
+      y = random.Next() % 500;
+      xVel = (random.Next() % 300) - 150;
+      yVel = (random.Next() % 300) - 150;
+    }
+
+    public bool Bounce()
+    {
+      int xLimit = 500;
+      int yLimit = 500;
+      bool bounced = false;
+
+      x += xVel;
+      y += yVel;
+      if (x > xLimit)
+      {
+        x = xLimit;
+        xVel = 0 - Math.Abs(xVel);
+        bounced = true;
+      }
+
+      if (x < 0)
+      {
+        x = 0;
+        xVel = Math.Abs(xVel);
+        bounced = true;
+      }
+
+      if (y > yLimit)
+      {
+        y = yLimit;
+        yVel = 0 - Math.Abs(yVel);
+        bounced = true;
+      }
+
+      if (y < 0)
+      {
+        y = 0;
+        yVel = Math.Abs(yVel);
+        bounced = true;
+      }
+
+      return bounced;
+    }
+  }
+
   public override object Execute()
   {
     Random random = new Random();
