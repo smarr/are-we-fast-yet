6d035e876a7dabc8bda1998d3ac767edad9aaff0
Stefan Marr <git@stefan-marr.de>
2022-10-25 17:30:22 +0100

    Add bable to webpack config to be able to compile to ECMAScript 5
    Just running `npm install . && npm run webpack` will generate a plain es5 compatible dist/harness.js file.
    
    Without bable, we would get a single file version of AWFY, which may also be useful for some runtime systems.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/JavaScript/webpack.config.js
+++ benchmarks/JavaScript/webpack.config.js
@@ -9,5 +9,21 @@ module.exports = {
   output: {
     filename: 'harness.js',
     path: path.resolve(__dirname, 'dist'),
+    chunkFormat: 'commonjs'
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  target: 'es5'
 };
