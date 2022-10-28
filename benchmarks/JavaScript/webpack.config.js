013d1ecb243bd7ab3d8d3d31db249bd7820b378f
Stefan Marr <git@stefan-marr.de>
2022-10-28 15:17:18 +0100

    Adapt webpack config to produce ES5 and ES2022 files, and add README to document it
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/JavaScript/webpack.config.js
+++ benchmarks/JavaScript/webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
-module.exports = {
+const es5 = {
   mode: 'production',
   optimization: {
     minimize: false
   },
   entry: './harness.js',
   output: {
-    filename: 'harness.js',
+    filename: 'harness.es5.js',
     path: path.resolve(__dirname, 'dist'),
     chunkFormat: 'commonjs'
   },
@@ -27,3 +27,19 @@ module.exports = {
   },
   target: 'es5'
 };
+
+const es2022 = {
+  mode: 'production',
+  optimization: {
+    minimize: false
+  },
+  entry: './harness.js',
+  output: {
+    filename: 'harness.es2022.js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFormat: 'commonjs'
+  },
+  target: 'es2022'
+};
+
+module.exports = [es5, es2022];
