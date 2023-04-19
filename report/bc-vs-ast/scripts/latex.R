c0f90bd7403397375c5c133858d1014f0e7dbf74
Stefan Marr <git@stefan-marr.de>
2023-04-19 18:17:06 +0100

    Reduce code duplication, and add a convenience macro for use in latex

--- report/bc-vs-ast/scripts/latex.R
+++ report/bc-vs-ast/scripts/latex.R
@@ -2,6 +2,24 @@ Cmd <- function(name, value) {
   paste0("\\newcommand{\\", name, "}{", value, "\\xspace}")
 }
 
+CmdsMedianMinMaxX <- function(cmd_prefix, data) {
+  median_cmd <- paste0(cmd_prefix, "MedianX")
+  min_cmd    <- paste0(cmd_prefix, "MinX")
+  max_cmd    <- paste0(cmd_prefix, "MaxX")
+  
+  c(
+    Cmd(median_cmd, X2(data$median)),
+    Cmd(max_cmd,    X2(data$max)),
+    Cmd(min_cmd,    X2(data$min)),
+    
+    Cmd(paste0(cmd_prefix, "MMMX"),
+        paste0("\\", median_cmd,
+               " (min.\\ \\", min_cmd,
+               ", max.\\ \\", max_cmd, ")")),
+    ""
+  )
+}
+
 X2 <- function(num) {
   paste0(format(num, digits=2, nsmall = 2), "\\texttimes")
 }
