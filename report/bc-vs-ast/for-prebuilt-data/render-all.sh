c461dba67b2d8dde94aa4311fb301e498e6d4672
Stefan Marr <git@stefan-marr.de>
2023-07-13 23:07:35 +0100

    Sec. 5.6

--- report/bc-vs-ast/for-prebuilt-data/render-all.sh
+++ report/bc-vs-ast/for-prebuilt-data/render-all.sh
@@ -3,4 +3,4 @@
 ../scripts/knit.R sec54-warmup.Rmd
 ../scripts/knit.R sec551-overall-mem-impact.Rmd
 ../scripts/knit.R sec552-mem-prog-representation.Rmd
-../scripts/knit.R individual-optimisations.Rmd
+../scripts/knit.R sec56-optimizations.Rmd
