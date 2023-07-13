5991e261cad8dc0c31fc0c43443bf172e25ba9c5
Octave Larose <octave.larose@hotmail.fr>
2023-07-13 16:07:17 +0100

    the great awfy purge of 2023 (many files died)

--- implementations/build-csom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ./script.inc
-source ./config.inc
-
-INFO Build CSOM
-cd CSOM
-make
-OK CSOM Build Completed.
--- report/scripts/colors.R
+++
@@ -1,5 +0,0 @@
-colors <- read.table("colors.csv", sep=",", header=FALSE)
-
-get_color <- function(hue, lightness) {
-    paste0("#", colors[[lightness]][[hue + 1]])
-}
