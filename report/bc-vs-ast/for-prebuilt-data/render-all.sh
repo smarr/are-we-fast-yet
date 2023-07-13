07bd772ef7ed3185dfbf5cd7d0c258e3ec8a617b
Stefan Marr <git@stefan-marr.de>
2023-07-13 22:00:17 +0100

    Adapt overview to match to sec. 5.2 and 5.3

--- report/bc-vs-ast/for-prebuilt-data/render-all.sh
+++ report/bc-vs-ast/for-prebuilt-data/render-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+../scripts/knit.R sec52-53-overview.Rmd
 ../scripts/knit.R individual-optimisations.Rmd
 ../scripts/knit.R memory_usage_analysis.Rmd
-../scripts/knit.R overview.Rmd
 ../scripts/knit.R smarr-mem-use.Rmd
 ../scripts/knit.R warmup.Rmd
