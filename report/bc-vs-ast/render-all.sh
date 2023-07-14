515caf055e662731ed333a82cea55fae8db30757
Octave Larose <octave.larose@hotmail.fr>
2023-07-14 14:32:00 +0100

    render-all.sh for non prebuilt experiments

---
+++ report/bc-vs-ast/render-all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./scripts/knit.R sec52-53-overview.Rmd
+./scripts/knit.R sec54-warmup.Rmd
+./scripts/knit.R sec551-overall-mem-impact.Rmd
+./scripts/knit.R sec552-mem-prog-representation.Rmd
+./scripts/knit.R sec56-optimizations.Rmd
