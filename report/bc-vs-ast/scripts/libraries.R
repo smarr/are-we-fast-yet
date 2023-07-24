c08558848863a0956a63cba8552168530458aaef
Stefan Marr <git@stefan-marr.de>
2023-07-24 23:41:10 +0100

    Split install-libraries.R out

--- report/bc-vs-ast/scripts/libraries.R
+++ report/bc-vs-ast/scripts/libraries.R
@@ -1,32 +1,6 @@
 writeLines("[INFO] Loading Libraries")
 
-load_and_install_if_necessary <- function(package_name) {
-  if (!suppressPackageStartupMessages(library(package_name, character.only=TRUE, logical.return=TRUE))) {
-    cat(paste0("Package ", package_name, " not found. Will install it."))
-    install.packages(package_name, repos='https://cloud.r-project.org/')
-    library(package_name, character.only=TRUE)
-  }
-}
-
-load_and_install_if_necessary("plyr")
-load_and_install_if_necessary("dplyr")
-load_and_install_if_necessary("ggplot2")
-load_and_install_if_necessary("tables")
-load_and_install_if_necessary("reshape2")
-load_and_install_if_necessary("assertthat")
-load_and_install_if_necessary("scales")
-load_and_install_if_necessary("RColorBrewer")
-
-load_and_install_if_necessary("forcats")
-
-load_and_install_if_necessary("qs")
-load_and_install_if_necessary("knitr")
-load_and_install_if_necessary("markdown")
-load_and_install_if_necessary("rmarkdown")
-load_and_install_if_necessary("viridis")
-
-load_and_install_if_necessary("gcookbook")
-
+source("install-libraries.R")
 source("data-processing2.R")
 source("plots.R")
 source("colors.R")
