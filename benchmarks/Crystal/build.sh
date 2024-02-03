c8a455313873a02a77acc23fc86eb5172c99ad20
Stefan Marr <git@stefan-marr.de>
2024-02-03 16:49:23 +0000

    Apply suggestions from shellcheck
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- benchmarks/Crystal/build.sh
+++ benchmarks/Crystal/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # make script fail on first error
-SCRIPT_PATH=$(dirname $0)
-source $SCRIPT_PATH/../script.inc
+SCRIPT_PATH="$(dirname "$0")"
+source "$SCRIPT_PATH/../script.inc"
 
 if [[ "$1" = "style" ]]
 then
@@ -9,5 +9,5 @@ then
 fi
 
 INFO Build Crystal Benchmarks
-pushd $SCRIPT_PATH
+pushd "$SCRIPT_PATH"
 crystal build --release --no-debug harness.cr
--- benchmarks/Ruby/build.sh
+++ benchmarks/Ruby/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e # make script fail on first error
-SCRIPT_PATH=$(dirname $0)
-source $SCRIPT_PATH/../script.inc
+SCRIPT_PATH="$(dirname "$0")"
+source "$SCRIPT_PATH/../script.inc"
 
 if [[ "$1" = "style" ]]
 then
   INFO Check style of Ruby benchmarks
-  pushd $SCRIPT_PATH
+  pushd "$SCRIPT_PATH"
   rubocop
 else
   exit 0
